2ffc78a1923b7749b0ae92618916f7d6d1772200
unknown <turnern1@PSLW25071.b2b.regn.net>
2014-09-25 14:14:17 +0100

    testing

--- test/helloTest.js
+++ test/helloTest.js
@@ -2,8 +2,11 @@
 var api = require('nodeunit-httpclient').create({
     port: 8080,
     path: '/api',   //Base URL for requests
-    status: 200
+    status: 200,    //Test each response is OK (can override later)
+    headers: {      //Test that each response must have these headers (can override later)
+        'content-type': 'text/plain'
     }
+
 });
 
 //Automatic tests on response object
