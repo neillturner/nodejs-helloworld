3f96eeacbadf495ea9110060f7f9e59a880ba62d
unknown <turnern1@PSLW25071.b2b.regn.net>
2014-09-25 13:21:26 +0100

    correct test

--- test/helloTest.js
+++ test/helloTest.js
@@ -2,7 +2,7 @@ exports.testHelloWorld = function(test) {
     test.expect(1);
     httputil(app.cgi(), function(server, client) {
         client.fetch('GET', '/', {}, function (resp) {
-            test.equals('Hello World'), resp.body);
+            test.equals('Hello World', resp.body);
             test.done();
         });
     });
