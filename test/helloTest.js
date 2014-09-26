a00499e0a726cf53ffdadb623e10cee6416f19c3
unknown <turnern1@PSLW25071.b2b.regn.net>
2014-09-26 10:39:08 +0100

    test

--- test/helloTest.js
+++ test/helloTest.js
@@ -1,14 +1,45 @@
-//Setup client with automatic tests on each response
-var api = require('nodeunit-httpclient').create({
-    host: 'localhost', 
-    port: 8080,
-    path: '/',   //Base URL for requests
-    status: 200,    //Test each response is OK (can override later)
-    headers: {      //Test that each response must have these headers (can override later)
-        'content-type': 'text/plain'
-    }
+var nodeunit = require('../lib/nodeunit/lib/nodeunit.js')
+var http = require('http');
+var logfile = require('../src/logfile.js');
 
-});
+var testmsg = 'Hello World';
+
+
+exports.testLogentry = function(test) {
+    test.expect(1); // make sure all the async expectations are fulfilled
+
+    var options = {
+        host: 'localhost',
+        port: 8080,
+        path: '/',
+        method: 'POST',
+        record_message: function(msg) {
+            console.log('received ' + msg + '\n');
+            test.equal(msg, testmsg);
+
+            logger.stop();
+            setTimeout(test.done, 2000);
+        }
+    };
+
+    var logger = logfile.loggerFactory(options);
+
+    logfile.write(testmsg, options);
+};
+
+
+
+//Setup client with automatic tests on each response
+//var api = require('nodeunit-httpclient').create({
+//    host: 'localhost', 
+//    port: 8080,
+//    path: '/',   //Base URL for requests
+//    status: 200,    //Test each response is OK (can override later)
+//    headers: {      //Test that each response must have these headers (can override later)
+//        'content-type': 'text/plain'
+//    }
+
+//});
 
 //Automatic tests on response object
 //exports.test1 = function(test) {
@@ -19,9 +50,9 @@ var api = require('nodeunit-httpclient').create({
 //    })
 //};
 
-exports.test1 = function(test) {
-    api.get(test, '/', function(res) {
-        test.equal(res.body,'Hello World');
-        test.done();
-    });
-};
+//exports.test1 = function(test) {
+//    api.get(test, '/', function(res) {
+//        test.equal(res.body,'Hello World');
+//        test.done();
+//    });
+//};
