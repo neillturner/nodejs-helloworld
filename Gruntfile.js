bba467527b075f3d83b42ded93d46d58b524fc29
unknown <turnern1@PSLW25071.b2b.regn.net>
2014-09-29 12:54:42 +0100

    wait

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
           wait: {
             options: {
-                delay: 300000
+                delay: 120000
           }},    
     
           nodeunit: {
