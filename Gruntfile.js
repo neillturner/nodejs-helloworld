ddaed49810d684bbcb34f34b306c0ebcf9ed1a88
unknown <turnern1@PSLW25071.b2b.regn.net>
2014-09-29 16:52:14 +0100

    exit

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
     //      },    
           nodeunit: {
             all: ['test/*Test.js']
-          }
+          },
           exit: {
             normal: {
           }
