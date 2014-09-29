740dffafdaab2c92b49f72b815ccca76c90a345c
unknown <turnern1@PSLW25071.b2b.regn.net>
2014-09-29 16:34:34 +0100

    no wait

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,35 +9,8 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', 'start');
   
-  grunt.initConfig({
-      wait: {
-          options: {
-              delay: 500
-          },
-          pause: {      
-              options: {
-                  before : function(options) {
-                      console.log('pausing %dms', options.delay);
-                  },
-                  after : function() {
-                      console.log('pause end');
-                  }
-              }
-          },
-          random: {      
-              options: {
-                  delay: 10,
-                  after : function() {
-                      console.log('gamble');
-                      return Math.random() < 0.05 ? false : true;
-                  }
-              }
-          }
-      }
-})
-
-    // Project configuration.                                                                                                                                                                
-    //grunt.initConfig({
+     // Project configuration.                                                                                                                                                                
+    grunt.initConfig({
     //      wait: {
     //        options: {
     //            delay: 60000
@@ -53,14 +26,14 @@ module.exports = function(grunt) {
 	//	       }
     //        }
     //      },    
-    //      nodeunit: {
-    //        all: ['test/*Test.js']
-    //      }
-    //});
+          nodeunit: {
+            all: ['test/*Test.js']
+          }
+    });
 
     // Load nodeunit task  
-    grunt.loadNpmTasks('grunt-wait');
-    //grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    //grunt.loadNpmTasks('grunt-wait');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
 };
 
