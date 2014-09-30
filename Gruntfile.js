37284dbcc1eca331ebf8a3de47000190f44856c0
unknown <turnern1@PSLW25071.b2b.regn.net>
2014-09-30 12:14:05 +0100

    syntax

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
           },
           passfail: {
              options: {
-                force: true
+                force: true,
              },
              all: {
                success: function() {
